a5d2875219571366803afd74fa340f5e61b89f0f
Daniel Codena <dacodena1@espe.edu.ec>
2026-05-29 19:05:22 -0500

    Add GUI v1.0

---
+++ 06Code/EducativeSoftware/src/main/java/ec/edu/espe/educativesoftware/EducativeSoftware.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espe.educativesoftware;
+
+import ec.edu.espe.educativesoftware.view.IScreen;
+
+/**
+ *
+ * @author Daniel Codena, CodeBreakers, @ESPE
+ */
+public class EducativeSoftware {
+
+    public static void main(String[] args) {
+        IScreen screen = new IScreen();
+        
+        screen.setVisible(true);
+        screen.setLocationRelativeTo(null);
+    }
+}
